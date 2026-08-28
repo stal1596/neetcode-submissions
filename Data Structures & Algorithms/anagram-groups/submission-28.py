d5e03423a355c6862e23fd4434fcb114bd782d82
StalinParoyds <140893964+stal1596@users.noreply.github.com>
2026-08-28 19:43:23 +0530

    Add: anagram-groups - submission-28

---
+++ Data Structures & Algorithms/anagram-groups/submission-28.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashmap = defaultdict(list)
+        for s in strs:
+            sorts = ''.join(sorted(s))
+            hashmap[sorts].append(s)
+        return list(hashmap.values())
